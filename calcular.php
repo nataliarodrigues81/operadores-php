768abd878ad8a82a91716ea242c83841ab9e12f7
nataliarodrigues81 <rodrigueznati2006@gmail.com>
2026-09-17 12:47:32 -0300

    Acidionando operadores lógicos

--- calcular.php
+++ calcular.php
@@ -6,8 +6,14 @@ $n2 = $_POST['n2'];
 $adicao = $n1 + $n2;
 $subtracao = $n1 - $n2;
 $multiplicacao = $n1 * $n2;
-$divisao = $n1 / $n2;
-$modulo = $n1 % $n2;
+
+$divisaoValida = $n2 != 0; // atribuir o resultado lógico na variavel
+if ($divisaoValida) 
+{
+    $divisao = $n1 / $n2;
+    $modulo = $n1 % $n2;
+}
+
 $potencia = $n1 ** $n2;
 $concatenacao = $n1.$n2; // o ponto JUNTA o texto, não soma como número
 
@@ -36,12 +42,26 @@ $concatenacao = $n1.$n2; // o ponto JUNTA o texto, não soma como número
             <?php echo "$n1 x $n2 = $multiplicacao";?>
         </h3>
         <h3>
-            <?php echo "$n1 ÷ $n2 = ".number_format($divisao,2,',','.');?>
+            <?php
+                if ($divisaoValida==true) 
+                {
+                    echo "$n1 ÷ $n2 = ".number_format($divisao,2,',','.');
+                    echo "<br>";
+                    echo "$n1 modulo $n2 = ".number_format($modulo,2,',','.');
+                } else 
+                {
+                    echo "<h1>Não há divisão por zero</h1>";
+                }
+             ?>
         </h3>
         <h3>
             <?php echo "$n1 <sup> $n2 </sup> = ".number_format($potencia,2,',','.');?>
         </h3>
-        <a href="index.php">voltar</a>
+        <h3>
+            <?php echo "$n1 concatenando $n2 = $concatenacao" ?>;
+        </h3>
+        
+        <a href="index.php">Voltar</a>
 
         
 
